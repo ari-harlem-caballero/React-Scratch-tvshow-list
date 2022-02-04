d07dc9ae80d13331227a36d19437c985a92a8666
ari-harlemcaballero <ari.bash.harlem@gmail.com>
2022-02-04 09:52:55 -0800

    created newShow func

--- src/App.js
+++ src/App.js
@@ -2,14 +2,42 @@ import './App.css';
 import { useState } from 'react';
 
 function App() {
-  // state: allShows, filteredShows, showSeasons, showCreator, showColor, *showActors[]*
+  // state: allShows, filteredShows, showTitle, showSeasons, showCreator, showColor, *showActors[]*
   const [allShows, setAllShows] = useState([]);
   const [filteredShows, setFilteredShows] = useState([]);
+  const [showFormTitle, setShowFormTitle] = useState('');
   const [showFormSeasons, setShowFormSeasons] = useState(1);
   const [showFormCreator, setShowFormCreator] = useState('');
   const [showFormColor, setShowFormColor] = useState('yellow');
 
-  // functions: filter, delete
+  // functions: filter, delete, submit(new)
+  function handleFilterShows(search) {
+
+  }
+
+  function handleDeleteShow(id) {
+
+  }
+
+  function submitShow(e) {
+    e.preventDefault();
+
+    const newShow = {
+      id: (Math.ceil(Math.random() * 100)),
+      title: showFormTitle,
+      seasons: showFormSeasons,
+      creator: showFormCreator,
+      color: showFormColor
+    };
+    // update array/add new, clear state
+    setAllShows([...allShows, newShow]);
+
+    setShowFormTitle('');
+    setShowFormSeasons('1');
+    setShowFormCreator('');
+    setShowFormColor('yellow');
+  }
+
   return (
     <div className="App">
       <section className='top'>
