03d701320791e316342883f773501bd44a7745ee
ari-harlemcaballero <ari.bash.harlem@gmail.com>
2022-02-04 10:19:02 -0800

    tvshowForm layout

--- src/TVShows/TVShowForm.js
+++ src/TVShows/TVShowForm.js
@@ -1,5 +1,21 @@
 import React from 'react';
 
-export default function TVShowForm() {
-  return <div></div>;
+export default function TVShowForm(props) {
+  return <div className='tv-show-form'>
+    <form>
+      <label></label>
+      <label></label>
+      <label></label>
+      <label>
+        <select>
+          <option></option>
+          <option></option>
+          <option></option>
+          <option></option>
+          <option></option>
+        </select>
+      </label>
+      <button></button>
+    </form>
+  </div>;
 }
