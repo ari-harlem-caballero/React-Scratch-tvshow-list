1154cb79e95e62e394459224548a108d7bd2c09e
ari-harlemcaballero <ari.bash.harlem@gmail.com>
2022-02-04 10:32:24 -0800

    created tvshow Form

--- src/TVShows/TVShowForm.js
+++ src/TVShows/TVShowForm.js
@@ -1,21 +1,31 @@
 import React from 'react';
 
 export default function TVShowForm(props) {
-  return <div className='tv-show-form'>
-    <form>
-      <label></label>
-      <label></label>
-      <label></label>
+  return <div className='tv-show-form-hold'>
+    <form className='tv-show-form' onSubmit={props.submitTVShow}>
       <label>
-        <select>
-          <option></option>
-          <option></option>
-          <option></option>
-          <option></option>
-          <option></option>
+          Title:
+        <input required value={props.showFormTitle} onChange={(e) => props.setShowFormTitle(e.target.value)}/>
+      </label>
+      <label>
+          Seasons:
+        <input required type="number" value={props.showFormSeasons} onChange={(e) => props.setShowFormSeasons(e.target.value)}/>
+      </label>
+      <label>
+          Creator:
+        <input required value={props.showFormCreator} onChange={(e) => props.setShowFormCreator(e.target.value)} />
+      </label>
+      <label>
+          Color:
+        <select required value={props.showFormColor} onChange={(e) => props.setShowFormColor(e.target.value)}>
+          <option value="yellow">Yellow</option>
+          <option value="red">Red</option>
+          <option value="lightblue">Blue</option>
+          <option value="lightseagreen">Green</option>
+          <option value="yellowgreen">Bright</option>
         </select>
       </label>
-      <button></button>
+      <button>Add Movie</button>
     </form>
   </div>;
 }
