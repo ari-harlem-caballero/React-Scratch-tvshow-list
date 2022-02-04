1d4ce0cf703edac5a0b8d84cc43342a0d38a449e
ari-harlemcaballero <ari.bash.harlem@gmail.com>
2022-02-04 09:57:31 -0800

    tvshow.js created

--- src/TVShows/TVShow.js
+++ src/TVShows/TVShow.js
@@ -1,5 +1,9 @@
 import React from 'react';
 
-export default function TVShow() {
-  return <div></div>;
+export default function TVShow(props) {
+  return <div className='tv-show' style={{ backgroundColor: props.tvshow.color }}>
+    <h3>{props.tvshow.title}</h3>
+    <p>Seasons: {props.tvshow.seasons}</p>
+    <p>Creator: {props.tvshow.creator}</p>
+  </div>;
 }
