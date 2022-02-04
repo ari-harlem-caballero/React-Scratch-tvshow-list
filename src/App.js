7485b1a08150312ae6000ed72033dc6334727143
ari-harlemcaballero <ari.bash.harlem@gmail.com>
2022-02-04 09:44:43 -0800

    set state

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  // state: allShows, filteredShows, showSeasons, showCreator, showColor, *showActors[]*
+  const [allShows, setAllShows] = useState([]);
+  const [filteredShows, setFilteredShows] = useState([]);
+  const [showFormSeasons, setShowFormSeasons] = useState(1);
+  const [showFormCreator, setShowFormCreator] = useState('');
+  const [showFormColor, setShowFormColor] = useState('yellow');
+
+  // functions: filter, delete
   return (
     <div className="App">
       <section className='top'>
