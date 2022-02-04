cea7991714b6f7a679c84bb03babfbe3664e199c
ari-harlemcaballero <ari.bash.harlem@gmail.com>
2022-02-04 10:12:24 -0800

    created tvshow list

--- src/TVShows/TVShowList.js
+++ src/TVShows/TVShowList.js
@@ -1,5 +1,11 @@
 import React from 'react';
+import TVShow from './TVShow';
 
-export default function TVShowList() {
-  return <div></div>;
+export default function TVShowList(props) {
+  return <div className='tv-show-list'>
+    {props.tvshow.map((tvshow, i) => 
+      <TVShow key={`${tvshow}-${i}`} 
+        tvshow={tvshow} />
+    )}
+  </div>;
 }
